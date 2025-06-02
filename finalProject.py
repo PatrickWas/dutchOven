d164ef35e1de98154a286fd2273c0acd3b178282
jherman50 <144398248+jherman50@users.noreply.github.com>
2025-06-02 15:34:04 -0400

    the buttons and sliders work

--- finalProject.py
+++ finalProject.py
@@ -1,28 +1,29 @@
 Web VPython 3.2
 
+running = True
+
+def Run(b):
+    global running
+    running = not running
+    if running: 
+        b.text = "Pause"
+        choices.disabled = True
+        tempslider.disabled = True
+        partslider.disabled = True
+        volslider.disabled = True
+        choices.selected = "Choose a variable"
+    else: 
+        b.text = "Run"
+        choices.disabled = False
+    
+button(text = "Pause", pos = scene.title_anchor, bind = Run)
+
 mass = 2.6567e-26
 rad = 6.6e-11
 V = (rad*10)**3
 n = 5
 T = 30
 R = 0.0821
-
-box(vol=V, size=vec(5,5,5), color=color.black, opacity=0.4)
-
-go = True
-
-alderaans = []
-for i in range(n-1):
-    alderaans.append(sphere(m=mass, pos=vec((1.5*i)-2,1,0), texture=textures.earth))
-
-deathStar = sphere(m=mass, pos=vec(2,-2,0), texture="https://i.imgur.com/S9WXwf2.png")
-
-tempslider = slider(bind=bindtemp, min=0, max=100, value = T)
-tempslider_caption = wtext(text="Temperature\n")
-partslider = slider(bind=bindpart, min=1, max=100, value = n)
-partslider_caption = wtext(text="Num. of particles\n")
-volslider = slider(bind=bindvol, value = V)
-volslider_caption = wtext(text="Volume\n")
 
 def bindtemp(evt):
     global T
@@ -36,18 +37,41 @@ def bindvol(evt):
     global V
     V=evt.value
 
-t = 0
-dt = 0.01
+tempslider = slider(bind=bindtemp, min=0, max=100, value = T)
+tempslider_caption = wtext(text="Temperature\n")
+partslider = slider(bind=bindpart, min=1, max=100, value = n)
+partslider_caption = wtext(text="Num. of particles\n")
+volslider = slider(bind=bindvol, value = V)
+volslider_caption = wtext(text="Volume\n")
+
+tempslider.disabled = True
+partslider.disabled = True
+volslider.disabled = True
+
+def M(m):
+    global tempslider, partslider, volslider, running
+    tempslider.disabled = True
+    partslider.disabled = True
+    volslider.disabled = True
+    val = m.selected
+    if val == "Temperature": 
+        tempslider.disabled = False
+    elif val == "Number of particles": 
+        partslider.disabled = False
+    elif val == "Volume": 
+        volslider.disabled = False
+
+choices = menu(choices=['Choose a variable', 'Temperature', 'Number of particles', 'Volume'], index=0, bind=M)
+choices.disabled = True
+
 wowT = label(pos=vec(0, 0, 0), text="temp: " + T, xoffset=20, yoffset=100, space=30, height=16, border=4, font='sans')
 wowN = label(pos=vec(0, 0, 0), text="num: " + n, xoffset=20, yoffset=-50, space=30, height=16, border=4, font='sans')
 wowV = label(pos=vec(0, 0, 0), text="vol: " + V, xoffset=-20, yoffset=50, space=30, height=16, border=4, font='sans')
+
+dt = 0.01
 while True:
     rate(1/dt)
     wowT.text = "temp: " + T
     wowN.text = "num: " + n
     wowV.text = "vol: " + V
-    if go:
-        for i in range(len(alderaans)):
-            alderaans[i].pos += vec(i, 0, 0)
-            deathStar.pos += vec(0.5, 0.5, 0.5)
-        t += dt
+    if running:
